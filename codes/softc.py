2eab6f0ee23eee9afc2942574fc4a05c254610eb
Yaswanthyash1 <147232443+Yaswanthyash1@users.noreply.github.com>
2024-05-06 16:26:44 +0530

    Update softc.py

--- codes/softc.py
+++ codes/softc.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
 # Extending SNR range from -5 to 50 dB
 snr_db_extended = np.linspace(-5, 50, 200)
 snr_lin_extended = 10 ** (snr_db_extended / 10)
@@ -13,13 +14,13 @@ throughput_with_combining_extended = 1 - np.exp(-snr_lin_extended / 3)
 
 # Plotting
 plt.figure(figsize=(10, 6))
-plt.plot(snr_db_extended, throughput_no_combining_extended, label='Without Soft Combining', color='brown')
-plt.plot(snr_db_extended, throughput_with_combining_extended, label='With Soft Combining', color='blue')
-plt.xlabel('SNR (dB)',fontweight='bold', fontsize=12)
-plt.ylabel('Throughput',fontweight='bold', fontsize=12)
-plt.xticks(fontsize=10, fontweight='bold')
-plt.yticks(fontsize=10, fontweight='bold')
-plt.title(' ')
+plt.plot(snr_db_extended, throughput_no_combining_extended, label='Without Soft Combining', color='red')
+plt.plot(snr_db_extended, throughput_with_combining_extended, label='With Soft Combining', color='blue', linestyle='dashdot')
+plt.xlabel('SNR (dB)',  fontsize=17)
+plt.ylabel('Throughput', fontsize=17)
+plt.xticks(fontsize=12)
+plt.yticks(fontsize=12 ) #fontweight='bold'
+plt.title('')
 plt.legend()
 plt.grid(False)
 plt.show()
