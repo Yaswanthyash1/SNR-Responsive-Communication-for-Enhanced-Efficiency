39789604d0640579ebcf628e7117c0460770d6dd
Yaswanthyash1 <147232443+Yaswanthyash1@users.noreply.github.com>
2024-05-06 16:27:10 +0530

    Update turbo.py

--- codes/turbo.py
+++ codes/turbo.py
@@ -11,15 +11,15 @@ throughput = 1 / (1 + np.exp(-0.3 * (snr_linear - 2)))
 
 # Plotting
 plt.figure(figsize=(10, 6))
-plt.plot(snr_db[snr_db <= 4], throughput[snr_db <= 4], color='black', linestyle='solid', label='Low SNR', linewidth=2)
-plt.plot(snr_db[snr_db > 4], throughput[snr_db > 4], color='black', linestyle='dotted', label='Moderate/High SNR', linewidth=2)
-plt.xticks(range(0, 18, 2), fontsize=10, fontweight='bold')
-plt.yticks(fontsize=10, fontweight='bold')
+plt.plot(snr_db[snr_db <= 4], throughput[snr_db <= 4], color='blue', linestyle='solid', label='Low SNR', linewidth=2)
+plt.plot(snr_db[snr_db > 4], throughput[snr_db > 4], color='red', linestyle='dotted', label='Moderate/High SNR', linewidth=2)
+plt.xticks(range(0, 18, 2), fontsize=12)
+plt.yticks(fontsize=12)
 plt.title(' ')
 
 # Make x-label and y-label bold and increase their size
-plt.xlabel('SNR (dB)', fontweight='bold', fontsize=12)
-plt.ylabel('Throughput (bpcu)', fontweight='bold', fontsize=12)
+plt.xlabel('SNR (dB)',fontsize=17)    #fontweight='bold'
+plt.ylabel('Throughput (bpcu)', fontsize=17)
 
 plt.legend()
 plt.grid(False)
